9ae3d7a24ffb507ecb7c8e22d32a6a399f788c20
Susumu Yamazaki <zacky1972@gmail.com>
2025-06-01 15:20:52 +0900

    fix: apply mix format --migrate

--- test/safe_native_test.exs
+++ test/safe_native_test.exs
@@ -8,9 +8,20 @@ defmodule SafeNativeTest do
       System.cmd("mix", ["deps.get"], cd: "test_support/sub2", into: IO.stream())
       System.cmd("mix", ["deps.get"], cd: "test_support/include_modules", into: IO.stream())
 
-      System.cmd("mix", ["deps.update", "--all"], cd: "test_support/sub1", into: IO.stream())
-      System.cmd("mix", ["deps.update", "--all"], cd: "test_support/sub2", into: IO.stream())
-      System.cmd("mix", ["deps.update", "--all"], cd: "test_support/include_modules", into: IO.stream())
+      System.cmd("mix", ["deps.update", "--all"],
+        cd: "test_support/sub1",
+        into: IO.stream()
+      )
+
+      System.cmd("mix", ["deps.update", "--all"],
+        cd: "test_support/sub2",
+        into: IO.stream()
+      )
+
+      System.cmd("mix", ["deps.update", "--all"],
+        cd: "test_support/include_modules",
+        into: IO.stream()
+      )
 
       {_, exit_code} = System.cmd("mix", ["test"], cd: "test_support/sub1", into: IO.stream())
       assert exit_code == 0
